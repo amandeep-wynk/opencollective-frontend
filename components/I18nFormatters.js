38d7dc61106afe1474fbf123a8879181f3b4a1d0
Nitin Kumar <snitin315@gmail.com>
2020-11-27 12:23:16 +0530

    feat: apply latest design to create organization form

--- components/I18nFormatters.js
+++ components/I18nFormatters.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Link from './Link';
 import StyledLink from './StyledLink';
 
 export const getI18nLink = linkProps => chunks => <StyledLink {...linkProps}>{chunks}</StyledLink>;
@@ -9,5 +10,16 @@ export const I18nSupportLink = chunks => (
   <StyledLink href="mailto:support@opencollective.com">{chunks.length || 'support@opencollective.com'}</StyledLink>
 );
 
-const I18nFormatters = { strong: I18nBold, i: I18nItalic, SupportLink: I18nSupportLink };
+export const I18nSignLink = msg => <Link route="signin">{msg}</Link>;
+export const I18nTOSLink = msg => <Link route="tos">{msg}</Link>;
+export const I18nPrivacyLink = msg => <Link route="privacypolicy">{msg}</Link>;
+
+const I18nFormatters = {
+  strong: I18nBold,
+  i: I18nItalic,
+  SupportLink: I18nSupportLink,
+  SignInLink: I18nSignLink,
+  TOSLink: I18nTOSLink,
+  PrivacyPolicyLink: I18nPrivacyLink,
+};
 export default I18nFormatters;
